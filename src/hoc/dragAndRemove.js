711e7ea2b5d3a7a986b2e1b660be882acd96e1ef
Oguz Vuruskaner <ovuruska@outlook.com>
2022-02-18 19:20:43 +0300

    Improved code readability.

--- src/hoc/dragAndRemove.js
+++ src/hoc/dragAndRemove.js
@@ -28,40 +28,33 @@ export default (WrappedComponent) => {
         onStart = (event) => {
             const {dragStart} = this.state
 
-            if(dragStart === null){
+            if (dragStart === null) {
                 this.setState({
                     dragStart: this.getClientX(event)
                 })
-
-            }else{
-
             }
 
         }
 
         getClientX = (event) => {
 
-            if( !!event.clientX && typeof event.clientX === "number"){
+            if (!!event.clientX && typeof event.clientX === "number") {
                 return event.clientX
-            }else if(event.changedTouches.length >= 1){
-
+            } else if (event.changedTouches.length >= 1) {
                 return event.changedTouches[0].clientX
-            }else {
+            } else {
                 return event.touches[0].clientX
             }
         }
 
         onStop = (event) => {
-            const {dragStart} = this.state
-            if(dragStart === null){
-                return
-            }
-            const dragPercentage = this.getDragPercentage(event)
-            const {onRemove,threshold = .25} = this.props
 
-            if(dragPercentage < threshold){
+            const dragPercentage = this.getDragPercentage(event)
+            const {onRemove, threshold = .25} = this.props
+
+            if (dragPercentage < threshold) {
                 onRemove(this.props)
-            }else{
+            } else {
                 this.setState({
                     dragStart: null,
                     opacity: 1
@@ -70,12 +63,6 @@ export default (WrappedComponent) => {
 
         }
         onDrag = (event) => {
-
-            const {dragStart} = this.state
-            if(dragStart === null){
-                return
-            }
-
             const dragPercentage = this.getDragPercentage(event)
 
             this.setState({
@@ -89,7 +76,7 @@ export default (WrappedComponent) => {
             const dragHandlers = {onStart: this.onStart, onStop: this.onStop, onDrag: this.onDrag};
             const {opacity} = this.state
 
-            return <Draggable cancel={"input , button"} position={{x:0,y:0}} axis={"x"} {...dragHandlers}>
+            return <Draggable cancel={"input , button"} position={{x: 0, y: 0}} axis={"x"} {...dragHandlers}>
                 <div style={{opacity}}>
                     <WrappedComponent {...this.props}/>
                 </div>
