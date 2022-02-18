0c832bf05df416659810da7565238f0881b2bcd0
Oguz Vuruskaner <ovuruska@outlook.com>
2022-02-18 19:28:46 +0300

    Logging disabled.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
 import {createStore, applyMiddleware} from "redux"
 import createSagaMiddleware from "redux-saga"
-import {createLogger} from "redux-logger"
 import reducers from "./redux/reducers"
 import sagas from "./redux/sagas"
 
 
 const sagaMiddleware = createSagaMiddleware()
-const logger = createLogger()
 
 const store = createStore(
     reducers,
-    applyMiddleware(sagaMiddleware, logger),
+    applyMiddleware(sagaMiddleware),
 )
 
 sagaMiddleware.run(sagas)
